4b074da94956c2c41f86eda9d2b16c2ef0ae28f4
labressler <liam.bressler@yale.edu>
2017-01-18 15:40:43 -0500

    Update qlearn.R

--- R/qlearn.R
+++ R/qlearn.R
@@ -24,8 +24,6 @@ qlearn <- function(game,statevars,possibleactions,playername="P1",numiter=1000,p
     {
       statevec <<- append(statevec,get(jj))
     }
-    allstatevec <<- vector()
-    allactionvec <<- vector()
     currentstate <<- paste(statevec,collapse=" ")
     allstatevec <<- append(allstatevec,currentstate)
     assign(playername,qlearningaction(q=initial,currentstate=currentstate))
@@ -33,25 +31,29 @@ qlearn <- function(game,statevars,possibleactions,playername="P1",numiter=1000,p
   })
   for (ii in 1:numiter)
   {
+    statevec <<- vector()
+    allstatevec <<- vector()
+    allactionvec <<- vector()
     rew <- as.numeric(newgame(...))
-    for (statenum in length(allstatevec))
+    for (statenum in 1:length(allstatevec))
     {
       curstate <- allstatevec[statenum]
       curraction1 <- allactionvec[statenum]
-      initial <- qlearningupdate(initial,curstate,curraction1,rew)
+      #initial <- qlearningupdate(initial,curstate,curraction1,rew)
+      initial <- qlearningupdate(initial,curstate,curraction1,rew,rewardcount=.5/length(allstatevec))
     }
   }
-  return (initial)
+  return (initial[order(rownames(initial)),])
 }
 
-qlearningupdate <- function(q,currentstate,currentaction,currentreward,nextstate=NULL,discount=.5,gamma=.25)
+qlearningupdate <- function(q,currentstate,currentaction,currentreward,nextstate=NULL,rewardcount=.5,gamma=.25)
 {
   rndn <- runif(1,0,1)
   if ((nrow(q) > 0) & (currentstate %in% rownames(q)))
   {
-    q[currentstate,currentaction] <- (q[currentstate,currentaction] + discount*currentreward)/(1+discount)
+    q[currentstate,currentaction] <- (1-rewardcount)*q[currentstate,currentaction] + rewardcount*currentreward
   } else {
-    qvec <- rep(0,ncol(q))
+    qvec <- rep(0.5,ncol(q))
     if(nrow(q)>0)
     {
       q <- rbind.data.frame(q,qvec)
@@ -63,7 +65,8 @@ qlearningupdate <- function(q,currentstate,currentaction,currentreward,nextstate
     {
       #print(paste0(currentstate,currentreward))
     }
-    q[currentstate,currentaction] <- (q[currentstate,currentaction] + discount*currentreward)/(1+discount)
+    #q[currentstate,currentaction] <- (q[currentstate,currentaction] + discount*currentreward)/(1+discount)
+    q[currentstate,currentaction] <- (1-rewardcount)*q[currentstate,currentaction] + rewardcount*currentreward
   }
   return(q)
 }
@@ -73,10 +76,14 @@ qlearningaction <- function(q,currentstate,exploration=.5)
   rndn <- runif(1,0,1)
   if (currentstate %in% rownames(q))
   {
-    vec <- q[currentstate,]
+    vec <- unlist(q[currentstate,])
+    vecs <- exp(log(vec/(1-vec)))
+    vecs <- vecs/mean(vecs)
     if (rndn<exploration)
     {
-      statenum <- sample.int(ncol(q),1)
+      #statenum <- sample.int(ncol(q),1,prob=exp((-1)/((vec+3/length(vec)/8)/(mean(vec)+3/8))))
+      #statenum <- sample.int(ncol(q),1,prob=exp(vec/mean(abs(vec))/2))
+      statenum <- sample.int(ncol(q),1,prob=exp(vec/mean(vecs)/2))
       retstate <- colnames(q)[statenum]
       return(retstate)
     } else {
